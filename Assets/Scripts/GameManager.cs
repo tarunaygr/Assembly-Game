85a212b1164139aa145bcdd50e858ec374bc3d93
tarunaygr <60793650+tarunaygr@users.noreply.github.com>
2022-05-28 17:04:39 +0530

    UI elements added. Table added to game scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    GameObject StartScreen, ConsentScreen,LevelScreen;
+
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("UI Manager");
+                go.AddComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+    public static int level { get; private set ; }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    private void Awake()
+    {
+        _instance= this;
+        StartScreen.SetActive(true);
+        ConsentScreen.SetActive(false);
+        LevelScreen.SetActive(false);
+    }
+
+    public void SetLevel(int l)
+    {
+        level = l;
+        Debug.Log(level);
+        LoadGame();
+    }
+}
