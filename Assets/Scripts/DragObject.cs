2ec9de4f669081261986e8b43e07e5fa1e994e5c
Adwaith-RK <adwaith02.rk@gmail.com>
2022-07-02 11:30:34 +0400

    inspect
    made bottom right cam to follow object clicked and combined drag and rotate to same script and debugged it

--- Assets/Scripts/DragObject.cs
+++ Assets/Scripts/DragObject.cs
@@ -8,6 +8,16 @@ public class DragObject : MonoBehaviour
     private float Zmouse;
     [SerializeField]
     private float objectheight=12f,max_width=13,max_height=6.5f;
+    public Camera Cam;
+    Vector3 prevPos = Vector3.zero;
+    Vector3 PosDelta = Vector3.zero;
+
+    void Start()
+    {
+        Cam = GameObject.Find("3D View Camera").GetComponent<Camera>();
+        if (Cam != null)
+            Debug.Log(Cam.rect);
+    }
     private Vector3 GetMouseWorldPos()
     {
         Vector3 mousePoint = Input.mousePosition;
@@ -29,16 +39,33 @@ public class DragObject : MonoBehaviour
 
         float xclamp=Mathf.Clamp(GetMouseWorldPos().x+Offset.x,-max_height,max_height);
         float zclamp = Mathf.Clamp(GetMouseWorldPos().z + Offset.z, -max_width, max_width);
-        transform.position = new Vector3(xclamp,objectheight,zclamp);
+        if (Input.GetMouseButton(0) && Input.mousePosition.x < (0.75 * Screen.width) && Input.mousePosition.y > (0.25 * Screen.height))
+        {
+            transform.position = new Vector3(xclamp, objectheight, zclamp);
+            Cam.transform.position = new Vector3(xclamp, Cam.transform.position.y, zclamp);
+        }
 
     }
- /*   private void OnMouseOver()
+    void Update()
     {
-        gameObject.transform.localScale = new Vector3(2,2,2);
+        if (Input.GetMouseButton(0) && Input.mousePosition.x > (Cam.rect.x * Screen.width) && Input.mousePosition.y < ((1-Cam.rect.y) * Screen.height))
+        {
+            PosDelta = Input.mousePosition - prevPos;
+
+            transform.Rotate(transform.right, -Vector3.Dot(PosDelta, Camera.main.transform.up), Space.World);
+
+        }
+        prevPos = Input.mousePosition;
     }
-    private void OnMouseExit()
-    {
-        gameObject.transform.localScale = new Vector3(1, 1, 1);
-    }
- */
+    /*   private void OnMouseOver()
+       {
+           gameObject.transform.localScale = new Vector3(2,2,2);
+       }
+    */
+       private void OnMouseExit()
+       {
+           transform.rotation = Quaternion.identity;
+       }
+    
+   
 }
